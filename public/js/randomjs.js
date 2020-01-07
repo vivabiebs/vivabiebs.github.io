3c1d72f9d18d3a1b4f1201c7f313339e8b43394d
vivabiebs <48383689+vivabiebs@users.noreply.github.com>
2020-01-08 00:01:23 +0700

    Update randomjs.js

--- public/js/randomjs.js
+++ public/js/randomjs.js
@@ -12,6 +12,7 @@ function get(){
         console.log(members[j]);
     }
     textInput.value = '';
+
 }
 
 function addInput(){
@@ -22,22 +23,23 @@ function addInput(){
     
     let inputField = document.createElement("input");
     inputField.setAttribute("type","text");
-    inputField.setAttribute("id","Name");
+    inputField.setAttribute("id","textInput");
 
     let submitBTN = document.createElement("button");
     let submit = document.createTextNode("submit");
     submitBTN.setAttribute("id","submitBTN");
-    submitBTN.setAttribute("style","font-family: 'Gochi Hand', cursive;")
+    submitBTN.setAttribute("style","font-family: 'Gochi Hand', cursive;background-color: lightpink;color: black;");
     submitBTN.setAttribute("onclick","get()");
     submitBTN.addEventListener("click",get);
     submitBTN.appendChild(submit)
 
     let addBTN = document.createElement("button");
     let add = document.createTextNode("+");
-    submitBTN.setAttribute("id","addInputBTN");
-    submitBTN.setAttribute("onclick","addInput()");
-    addBTN.appendChild(add)
+    addBTN.setAttribute("id","addInputBTN");
+    addBTN.setAttribute("style","font-family: 'Gochi Hand', cursive;background-color: lightpink;color: black;");
+    addBTN.setAttribute("onclick","addInput()");
     addBTN.addEventListener("click",addInput);
+    addBTN.appendChild(add)
 
     document.getElementById("inputForm").appendChild(inputStr);
     document.getElementById("inputForm").appendChild(inputField);
@@ -51,15 +53,46 @@ function addNum(){
    giftNums = parseInt(document.getElementById("numInput").value);
    console.log(giftNums);
 }
-// numInput.addEventListener("click",addNum);
 
+let numBuffer = [];
+let luckyName = [];
+let luckyLabel = document.getElementById("lucky");
 
 function random(){
-
+    numBuffer = [];
+    luckyName = [];
+    if (members.length == 0) {
+        alert("No Participants");
+    }
+    if (document.getElementById("numInput").value == 0) {
+        alert("Please input gifts number");
+    }
+    else {
+        for (i = 0 ; i < document.getElementById("numInput").value ; i++) {
+            if ( i == members.length) {
+                break;
+            }
+            let temp = Math.floor(Math.random() * members.length);
+            if (numBuffer.includes(temp)) {
+                while (numBuffer.includes(temp)) {
+                    temp = Math.floor(Math.random() * members.length);
+                }
+                numBuffer.push(temp);
+                luckyName.push(members[temp]);
+            }
+            else {
+                numBuffer.push(temp);
+                luckyName.push(members[temp]);
+            }
+        }
+        console.log(numBuffer);
+        console.log(luckyName);
+    }
+    document.getElementById("luckyName").innerHTML = "";
+    luckyName.forEach(printAllLuckyName);
 }
 
-function creatTable(){
 
+function printAllLuckyName(item, index) {
+    document.getElementById("luckyName").innerHTML += "gift no." + (index + 1) + " : " + item + "<br>";
 }
-
-// addInputBTN.addEventListener("click",addInput());
